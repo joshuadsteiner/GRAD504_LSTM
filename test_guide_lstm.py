9a04805373e3b4ffb2c2908f09fbefe4c5a74f9e
joshuadsteiner <146394462+joshuadsteiner@users.noreply.github.com>
2025-05-04 16:29:05 -0500

    Add files via upload

---
+++ test_guide_lstm.py
@@ -0,0 +1,44 @@
+# test_guide.py
+
+# A module to centrally define the test phase information
+
+##################
+####  TRAINING ###
+##################
+def get_train_start(phase):
+    train_start_dates = {1.1:'2016-01-01',
+                         1.2:'2013-01-01'}            
+    return train_start_dates.get(phase)
+    
+def get_train_end(phase):
+    train_end_dates = {1.1:'2017-12-01',
+                       1.2:'2017-12-01'}            
+    return train_end_dates.get(phase)
+    
+    
+####################
+####  VALIDATION ###
+####################
+def get_val_start(phase):
+    val_start_dates = {1.1:'2018-01-01',
+                       1.2:'2017-01-01'}            
+    return val_start_dates.get(phase)
+    
+def get_val_end(phase):
+    val_end_dates = {1.1:'2018-12-01',
+                     1.2:'2018-12-01'}            
+    return val_end_dates.get(phase)
+
+
+##############
+####  TEST ###
+##############
+def get_test_start(phase):
+    test_start_dates = {1.1:'2019-01-01',
+                        1.2:'2018-01-01'}            
+    return test_start_dates.get(phase)
+    
+def get_test_end(phase):
+    test_end_dates = {1.1:'2019-12-01',
+                      1.2:'2019-12-01'}            
+    return test_end_dates.get(phase)
