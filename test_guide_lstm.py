87071503c6a1548d958a988a21e4b71bac2640a1
joshuadsteiner <146394462+joshuadsteiner@users.noreply.github.com>
2025-05-04 18:58:09 -0500

    Add files via upload

--- test_guide_lstm.py
+++ test_guide_lstm.py
@@ -10,13 +10,13 @@
 ####  TRAINING ###
 ##################
 def get_train_start(phase):
-    train_start_dates = {1:'2013-01-01',
-                         2:'2013-01-01'}            
+    train_start_dates = {1.1:'2013-01-01',
+                         1.2:'2013-01-01'}            
     return train_start_dates.get(phase)
     
 def get_train_end(phase):
-    train_end_dates = {1:'2017-12-01',
-                       2:'2020-12-01'}            
+    train_end_dates = {1.1:'2017-12-01',
+                       1.2:'2020-12-01'}            
     return train_end_dates.get(phase)
     
     
@@ -24,13 +24,13 @@ def get_train_end(phase):
 ####  VALIDATION ###
 ####################
 def get_val_start(phase):
-    val_start_dates = {1:'2017-01-01',
-                       2:'2020-01-01'}            
+    val_start_dates = {1.1:'2017-01-01',
+                       1.2:'2020-01-01'}            
     return val_start_dates.get(phase)
     
 def get_val_end(phase):
-    val_end_dates = {1:'2018-12-01',
-                     2:'2021-12-01'}            
+    val_end_dates = {1.1:'2018-12-01',
+                     1.2:'2021-12-01'}            
     return val_end_dates.get(phase)
 
 
@@ -38,11 +38,11 @@ def get_val_end(phase):
 ####  TEST ###
 ##############
 def get_test_start(phase):
-    test_start_dates = {1:'2018-01-01',
-                        2:'2021-01-01'}            
+    test_start_dates = {1.1:'2018-01-01',
+                        1.2:'2021-01-01'}            
     return test_start_dates.get(phase)
     
 def get_test_end(phase):
-    test_end_dates = {1:'2019-12-01',
-                      2:'2022-12-01'}            
+    test_end_dates = {1.1:'2019-12-01',
+                      1.2:'2022-12-01'}            
     return test_end_dates.get(phase)
